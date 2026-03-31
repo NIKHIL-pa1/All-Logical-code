72f91d18659beb680ffb6a26892b5dd61782ae1c
Nikhil <nikhilpatil6519@gmail.com>
2026-03-31 13:35:21 +0530

    New File

---
+++ All Code/Liskov Substitution Principle/Program.cs
@@ -0,0 +1,88 @@
+﻿class MyClass
+{
+    static void Main(string[] args)
+    {
+        //For bad
+        Bird bird = new Ostrich();
+
+       // bird.Fly();
+
+        //For Good
+        AllBirds b1 = new Sparrows();
+        b1.Eat();
+        AllBirds b2 = new Ostriches();
+        b2.Eat();
+        FlyingBirds b3 = new Sparrows();
+        b3.Flys();
+
+    }
+}
+
+#region Bad Example of LSP
+
+
+public class Bird
+{
+    public virtual void Fly()
+    {
+        Console.WriteLine("Bird is flying");
+    }
+}
+
+public class Sparrow : Bird
+{
+    public override void Fly()
+    {
+        Console.WriteLine("Sparrow is flying");
+    }
+}
+public class Ostrich : Bird
+{
+    public override void Fly()
+    {
+        throw new Exception("Ostrich cannot fly");
+
+    }
+}
+#endregion
+
+#region Good Example of LSP
+
+public class AllBirds
+{
+    public virtual void Eat()
+    {
+        Console.WriteLine("Birds are eats");
+    }
+    //public virtual void Flys()
+    //{
+    //    Console.WriteLine("Birds are eats");
+    //}
+}
+
+public class FlyingBirds : AllBirds
+{
+    public virtual void Flys()
+    {
+        Console.WriteLine("flying birds");
+    }
+}
+
+public class Sparrows : FlyingBirds
+{
+    public override void Flys()
+    {
+        Console.WriteLine("Sparrow is flying");
+    }
+}
+
+public class Ostriches : AllBirds
+{
+    public override void Eat()
+    {
+        Console.WriteLine("Ostrich is eating");
+
+    }
+}
+
+#endregion
---
+++ All Code/OpenClose/Program.cs
@@ -0,0 +1,86 @@
+﻿class Demo
+{
+    static void Main(string[] args)
+    {
+        Invoice obj = new Invoice();
+
+        IInterface obj2 = new RegularCust();
+
+        double discount = obj.Discount(obj2);
+        Console.WriteLine(discount);
+
+
+
+        Cust cust = new Cust();
+        Customer ob = new EmpPrimium();
+
+        Console.WriteLine(ob.GetDiscount()); 
+
+
+    }
+}
+#region Below example with inheritance
+
+interface IInterface
+{
+    double GetDiscount();
+}
+
+
+
+
+class RegularCust : IInterface
+{
+    public double GetDiscount() => 0.3;
+}
+
+class PremiumCust : IInterface
+{
+    public double GetDiscount() => 0.5;
+}
+
+class EliteCust : IInterface
+{
+    public double GetDiscount() => 0.7;
+}
+
+class Invoice
+{
+    public double Discount(IInterface dics)
+    {
+       return dics.GetDiscount();
+    }
+}
+// Above example with inheritance
+#endregion
+
+#region Below example with inheritance 
+
+public abstract class Customer
+{
+    public abstract double GetDiscount();
+}
+
+public class EmpRegular : Customer
+{
+    public override double GetDiscount() => 0.3;
+}
+
+public class EmpPrimium : Customer
+{
+    public override double GetDiscount() => 0.5;
+}
+
+public class EmpElite : Customer
+{
+    public override double GetDiscount() => 0.7;
+}
+
+public class Cust
+{
+    public double GetDisc(Customer customer)
+    {
+       return customer.GetDiscount();
+    }
+}
+#endregion
